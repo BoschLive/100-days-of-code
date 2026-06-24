87c65d75a89421d164bc44da0bccaa7d90ca63bc
BoschLive <116604832+BoschLive@users.noreply.github.com>
2026-06-24 15:46:26 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+###################################################
+## BAND NAME GENERATOR                           ##
+## Take input from user to generate a band name  ##
+###################################################
+
+first_word = input("What street do you live on? ")
+second_word = input("What is your first name? ")
+
+print("Your band name is: " + first_word + " " + second_word)
